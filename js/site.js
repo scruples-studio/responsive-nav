990b6178c2949f95c280828c31f01474547d46a3
unlikelyobjects <hello@unlikelyobjects.com>
2021-07-03 10:01:16 +0100

    v2

--- js/site.js
+++ js/site.js
@@ -1,7 +1,3 @@
 // Responsive nav
-// https://github.com/viljamis/responsive-nav.js
 // @codekit-append quiet "vendor/responsive-nav.js"
-
-/* Last
-   ------------------------------------------------------------ */
 // @codekit-append quiet "init.js";
